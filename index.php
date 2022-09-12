ad4d8b93b7cbaa80c0b95e21d2974f0f9915f158
Emma1805 <emunoz5498@misena.edu.co>
2022-09-12 13:50:55 -0500

    commit 4

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include("template/cabecera.php"); ?>
+               <div class="jumbotron text-center"> 
+               <img width="400" src="img/Boricueblog.png" class=" mx-auto d-block"/>
+                  <p class="lead">Bienvenidos a Boricue blog. El blog donde podran donar, intercambiar, vender y comprar residuos de segundo uso.</p>
+                  <hr class="my-2">
+                  <p>Más información</p>
+                  <p class="lead">
+                     <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacén</a>
+                  </p>
+                  <p>¿Quieres aportar algo? Registrese aqui:</p>
+                  <p class="lead">
+                     <a class="btn btn-primary btn-lg" href="http://localhost/Boricue%20blog/administrador/register.php" role="button">Registrarse</a>
+                  </p>
+               </div>
+             </div>
+         </div>
+         
+ 
+         </div> 
+<?php include("template/pie.php"); ?>                                  
+
+      
