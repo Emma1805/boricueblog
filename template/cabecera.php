a3757c5face7c5f11699f532312cab803b7dc68e
Emma1805 <emunoz5498@misena.edu.co>
2022-09-12 13:58:16 -0500

    commit 6

---
+++ template/cabecera.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+   <html lang ="es">
+    <head>
+      <meta charset="UTF-8"> 
+      <meta http-equiv="x-UA-"compactible content="IE=edge">
+      <meta name="viewport" content="widht=device-widht initial,scale=1.0">
+      <title>BORICUE BLOG</title> 
+      <link rel="stylesheet"href="./css/bootstrap.min.css"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Atma:wght@500&family=Roboto:wght@400&display=swap" rel="stylesheet">
+      <link rel="stylesheet"href="./css/estilos.css"/>
+      <style></style>          
+
+   </head> 
+       <body>
+         <nav  class="navbar  navbar-expand-lg navbar-dark bg-primary">
+            <ul class= "nav navbar-nav">
+               <br>
+               <li class="nav-item">
+                  <a class="nav-link" href="index.php">INICIO
+                  </a> 
+               </li> 
+               <li class="nav-item">
+                  <a class="nav-link" href="nosotros.php">NOSOTROS
+                  </a>
+               <li class="nav-item">
+                  <a class="nav-link" href="reciclaje.php">RECICLAJE
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="productos.php">PRODUCTOS
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="contacto.php">CONTACTO
+                  </a>
+               </li>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="http://localhost/Boricue%20blog/administrador/register.php">REGISTRARSE
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="http://localhost/Boricue%20blog/administrador/">INGRESAR
+                  </a>
+               </li>
+            </ul>                                    
+                      
+         </nav>
+</br>
+         <img src="img/logo5.png" width="150px" height="100px"/>
+         <div class="container">
+            <br/>
+             <div class="row">
---
+++ template/pie.php
@@ -0,0 +1,10 @@
+</body>     
+          
+          <main>
+             <h2></h2>
+             <p></p>
+          </main>
+          <footer>
+          </footer>
+       </body> 
+    </html>
