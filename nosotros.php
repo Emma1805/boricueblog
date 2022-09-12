f59e3f8a418a0f9fe9aad1bd62073f7e03e04282
Emma1805 <emunoz5498@misena.edu.co>
2022-09-12 13:46:29 -0500

    commit 3

---
+++ nosotros.php
@@ -0,0 +1,26 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Somos un grupo de investigacion asociados al Centro de Diseño y Manufactura del Cuero BIOMATI, buscamos encontrar desechos y 
+        reciduos del sector agricola e industrial de la region para aplicarlos en futuros desarrollos en los proyectos de las vigencias del año 2023 por esta razon 
+        queremos construir este blog para tener una base de datos sobre aquellos productos que podamos utilizar dentro de la formulacion 
+        del proyecto.
+    </p>
+</div>
+
+<div class="jumbotron">
+    <h1 class="display-3">Misión</h1>
+    <p class="lead">Somos un centro de acopio tecnológico dedicados a recopilar toda la información de empresas nacionales e internacionales que 
+        suministren, donen, vendan o Cuando la vida útil de un producto ha llegado a su fin, pasa a ser desechada y contamina el medio ambiente.
+        Uno de los principales motivos es hacernos responsables de la basura que generamos y aplicar el concepto de las cuatro erres Reciclar, Reducir, reutilizar y recuperar.</p>
+</div>
+
+<div class="jumbotron">
+    <h1 class="display-3">Visión</h1>
+    <p class="lead">Queremos ser un referente dentro del sector del reciclaje a nivel nacional e internacional, aportando toda la información para el intercambio 
+        entre empresas que generen productos y subproductos de segundo uso a nivel global.</p>
+</div>
+
+<?php include("template/pie.php"); ?>                                  
